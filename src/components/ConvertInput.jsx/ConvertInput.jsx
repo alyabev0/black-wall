57bba1969b550603b40d13d9555848641f6adcf6
alyabev0 <alyabev1998@gmail.com>
2022-10-25 02:03:01 +0300

    safe commit перед сплитом компонента dropdown

--- src/components/ConvertInput.jsx/ConvertInput.jsx
+++ src/components/ConvertInput.jsx/ConvertInput.jsx
@@ -4,10 +4,32 @@ import { Input } from "../Input/Input.jsx"
 import { Dropdown } from "../Dropdown/Dropdown.jsx"
 
 export const ConvertInput = () => {
+    const item1 = {
+        title: "item1",
+        value: 228
+    }
+    const item2 = {
+        title: "item2",
+        value: 1337
+    }
+    const item3 = {
+        title: "item2",
+        value: 1337
+    }
+    const item4 = {
+        title: "item2",
+        value: 1337
+    }
+    const item5 = {
+        title: "item2",
+        value: 1337
+    }
+    const items = [item1, item2, item3, item4, item5]
+
     return (
         <div className="convert-input">
             <Input placeholder={'Text'} />
-            <Dropdown />
+            <Dropdown items={items} />
         </div>
     )
 }
--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,11 +1,22 @@
 import "./Dropdown.scss"
 
 export const Dropdown = ({ items }) => {
+    const clickHandler = (e) => {
+
+    }
+
     return (
         <>
             <div className="dropdown">
                 <div className="dropdown-title">BTC</div>
-                <div className="dropdown-button"></div>
+                <div className="dropdown-button" onClick={clickHandler}></div>
+
+                <ul className="dropdown-items">
+                    {items.map((item) =>
+                        <li className="dropdown-item">{item.title}</li>
+                    )}
+                </ul>
+
             </div>
         </>
     )
