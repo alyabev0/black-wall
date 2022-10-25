50fd44f194fe59df20c8a3ec1022a1921703ed6b
alyabev0 <alyabev1998@gmail.com>
2022-10-26 01:40:29 +0300

    добавил мок api и запрос к нему

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+
+import { mockGetDirections, mockGetFilters } from "../../api/api";
 import { ConvertInput } from "../ConvertInput.jsx/ConvertInput";
 import "./App.scss";
 
 function App() {
-  return <div className="container">
-    <ConvertInput />
-  </div>;
+  const [convertData, setConvertData] = useState({})
+
+  useEffect(() => {
+    const getData = async () => {
+      const directions = await mockGetDirections();
+      const filters = await mockGetFilters()
+      setConvertData({
+        directions,
+        filters
+      })
+    }
+    getData()
+  }, [])
+
+  return (
+    <div className="container">
+      <ConvertInput />
+    </div>
+  )
 }
 
 export default App;
